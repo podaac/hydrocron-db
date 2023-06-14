563e5f3ccbcf6bf75ccd7e6e39201ebce46ae260
Victoria McDonald <victoria.mcdonald@jpl.nasa.gov>
2023-06-13 17:22:07 -0700

    code to add and retrieve items from db

--- hydrocron-db/hydrocron_table.py
+++ hydrocron-db/hydrocron_table.py
@@ -104,6 +104,172 @@ class Hydrocron_Table:
         else:
             return tables
         
+    def add_data(self, partition_key_name, **kwargs):
+        """
+        Adds a data item to the table.
+
+        Parameters
+        ---------
+        partition_key_name : string
+            The name of the partition key for the table
+        **kwargs: All attributes to add to the item. Must include partition and sort keys
+        """
+
+        item_dict = {}
+
+        for key, value in kwargs.items():
+            item_dict[key] = value
+
+        item_id = item_dict[partition_key_name]
+        try:
+            self.table.put_item(
+                Item=item_dict
+            )
+        except ClientError as err:
+            logger.error(
+                "Couldn't add item %s to table %s. Here's why: %s: %s",
+                partition_key_name, self.table.name,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+
+    def get_item(self, partition_key_name, partition_key, sort_key_name, sort_key):
+        """
+        Gets data from the table for a single item.
+
+        Parameters
+        ----------
+        partition_key_name : string
+            the name of the partition key
+        partition_key : string
+            the feature ID
+        sort_key_name : string
+            the name of the sort key, usually time
+        sort_key: string
+            the value of the sort key item.
+        
+        Returns
+        -------
+        The data about the requested item.
+
+        """
+        try:
+            item_key = {}
+            item_key[partition_key_name] = partition_key
+            item_key[sort_key_name] = sort_key
+
+            print(item_key)
+
+            response = self.table.get_item(Key=item_key)
+        except ClientError as err:
+            logger.error(
+                "Couldn't get item %s from table %s. %s: %s",
+                partition_key, self.table.name,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+        else:
+            return response['Item']
+        
+    def query(self, partition_key_name, partition_key, sort_key_name, sort_key):
+        """
+        Perform a query for multiple items.
+
+        Parameters
+        ----------
+        partition_key_name : string
+            the name of the partition key
+        partition_key : string
+            the feature id to query
+        sort_key_name : string
+            the name of the sort key, usually time
+        sort_key : integer
+            the value of the sort keys to query
+
+
+        Returns
+        -------
+            The item.
+
+        """
+        try:
+            response = self.table.query(
+                KeyConditionExpression=(Key(partition_key_name).eq(partition_key) & 
+                                      Key(sort_key_name).eq(sort_key)))
+        except ClientError as err:
+            logger.error(
+                "Couldn't query for movies released in %s. Here's why: %s: %s", year,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+        else:
+            return response['Items']
+        
+    def update_item(self, partition_key_name, partition_key, sort_key_name, sort_key, **kwargs):
+        """
+        Updates attribute data for a reach, node, or lake in the table.
+
+        Parameters
+        ----------
+        partition_key_name: string
+            The name of the partition key.
+        partition_key: string
+            The ID of the feature to update.
+        sort_key_name: string
+            The name of the sort key.
+        sort_key: string
+            The timestamp of the item to update.
+        **kwargs: The fields to update with their new values
+        
+        Returns
+        -------
+        The fields that were updated, with their new values.
+
+        """
+
+        update_expr = "set "
+        expr_att_values = {}
+
+        for key, value in kwargs.items():
+            update_expr = update_expr + "{}=:{}, ".format(key, key)
+            expr_att_values[":{}".format(key)] = value
+
+
+        try:
+            response = self.table.update_item(
+                Key={partition_key_name: partition_key, sort_key_name: sort_key},
+                UpdateExpression=update_expr,
+                ExpressionAttributeValues=expr_att_values,
+                ReturnValues="UPDATED_NEW")
+        except ClientError as err:
+            logger.error(
+                "Couldn't update item %s in table %s. %s: %s",
+                partition_key, self.table.name,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+        else:
+            return response['Attributes']
+        
+    def delete_item(self, partition_key_name, partition_key, sort_key_name, sort_key):
+        """
+        Deletes an item from the table.
+
+        Parameters
+        ----------
+        partition_key_name: string
+            The name of the partition key.
+        partition_key: string
+            The ID of the item to delete.
+        sort_key_name: string
+            The name of the sort key.
+        sort_key: string
+            The timestamp of the item to delete.
+        """
+        try:
+            self.table.delete_item(Key={partition_key_name: partition_key, sort_key_name: sort_key})
+        except ClientError as err:
+            logger.error(
+                "Couldn't delete item %s. %s: %s", partition_key,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+
 
     def delete_table(self):
         """
--- hydrocron-db/tests/test_hydrocron_table.py
+++ hydrocron-db/tests/test_hydrocron_table.py
@@ -4,19 +4,24 @@ test_create_table.py
 ==============
 Test creating a Hydrocron dynamodb table.
 
-Unit tests for creating tables in the Hydrocron Database. 
+Unit tests for creating tables and adding items to the Hydrocron Database. 
 Requires a local install of DynamoDB to be running. See https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.html
 
 """
 import pytest
 import boto3
+import json
+from decimal import Decimal
+import geopandas as gpd
 from botocore.exceptions import ClientError
 
 from hydrocron_table import Hydrocron_Table
 
 
-test_shapefile_path = '/data/SWOT_L2_HR_RiverSP_Node_540_010_AS_20230602T193520_20230602T193521_PIA1_01/SWOT_L2_HR_RiverSP_Node_540_010_AS_20230602T193520_20230602T193521_PIA1_01.shp'
+test_shapefile_path = '/tests/data/SWOT_L2_HR_RiverSP_Node_540_010_AS_20230602T193520_20230602T193521_PIA1_01/SWOT_L2_HR_RiverSP_Node_540_010_AS_20230602T193520_20230602T193521_PIA1_01.shp'
 test_table_name = 'hydrocron_test_table'
+test_partition_key_name = 'reach_id'
+test_sort_key_name = 'time'
 
 def test_create_table(dyndb_resource_connection, table_name=test_table_name):
     '''
@@ -59,6 +64,36 @@ def test_list_tables(dyndb_resource_connection):
     assert len(list_of_tables) > 0
     assert list_of_tables[0].table_name == test_table_name
 
+def test_add_data(dyndb_resource_connection):
+    hydrocron_test_table = Hydrocron_Table(dyn_resource=dyndb_resource_connection)
+            
+    # read shapefile into geopandas dataframe
+    shp_file = gpd.read_file(test_shapefile_path)
+
+    item_attrs = {}
+    for index, row in shp_file.iterrows():
+        # convert each reach into a dictionary of attributes that dynamo can read
+        item_atts = json.loads(row.to_json(default_handler=str), parse_float=Decimal)
+
+        # write to the table
+        hydrocron_test_table.add_data(partition_key_name=test_partition_key_name, **item_attrs)
+        break # just test one item
+
+    item = hydrocron_test_table.get_item(partition_key_name=test_partition_key_name,
+                                         partition_key=item_attrs[test_partition_key_name],
+                                         sort_key_name = test_sort_key_name,
+                                         sort_key = item_attrs[test_sort_key_name])
+    
+    assert item[test_partition_key_name] == test_partition_key_name
+
+def test_get_item(dyndb_resource_connection):
+    pass;
+
+def test_query(dyndb_resource_connection, partition_key_name):
+    pass;
+
+def test_delete_item(dyndb_resource_connection):
+    pass;
 
 def test_delete_table(dyndb_resource_connection):
     '''
