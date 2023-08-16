2e57daa50a39b24e0c622d36fc9eb879cb06f1fb
vggonzal <9Tcostoamm>
2023-08-15 19:06:42 -0700

    functioning local testing for dynamodb

---
+++ conftest.py
@@ -0,0 +1,13 @@
+'''
+conftest file to set up local dynamodb connection
+'''
+from pytest_dynamodb import factories
+from tests.database_fixtures import hydrocron_dynamo_instance
+
+__all__ = ['hydrocron_dynamo_instance']
+
+dynamo_test_proc = factories.dynamodb_proc(
+        dynamodb_dir="tests/dynamodb_local",
+        port=8001)
+
+dynamo_db_resource = factories.dynamodb("dynamo_test_proc")
