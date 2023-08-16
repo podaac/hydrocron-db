dd60b68470f5d7d35f50d653d912df063cef93e4
vggonzal <9Tcostoamm>
2023-08-15 22:39:14 -0700

    testing remote dynamodb

--- tests/example_load_data.py
+++ tests/example_load_data.py
@@ -1,21 +1,27 @@
 import boto3
 import json
+from decimal import Decimal
+import geopandas as gpd
 
 dynamodb = boto3.client('dynamodb')
 
 
 def upload():
-    song = "test01"
-    print(song)
-    item = {
-        'feature_id': {'id': '001'}
-    }
-    print(item)
-    response = dynamodb.put_item(
-        TableName='hydrocron_swot_reaches',
-        Item=item
-    )
-    print("UPLOADING ITEM")
+    test_shapefile_path = 'tests/data/SWOT_L2_HR_RiverSP_Reach_548_011_NA_20230610T193337_20230610T193344_PIA1_01/SWOT_L2_HR_RiverSP_Reach_548_011_NA_20230610T193337_20230610T193344_PIA1_01.shp'
+
+    # read shapefile into geopandas dataframe
+    shp_file = gpd.read_file(test_shapefile_path)
+
+    item_attrs = {}
+    for index, row in shp_file.iterrows():
+        # convert each reach into a dictionary of attributes that dynamo can read
+        item_attrs = json.loads(row.to_json(default_handler=str), parse_float=Decimal)
+
+        # write to the table
+        response = dynamodb.put_item(
+            TableName='hydrocron_swot_reaches',
+            Item=item_attrs
+        )
     print(response)
 
 
