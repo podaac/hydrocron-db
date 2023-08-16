050fa0ec14c146c8faf262ba9e13e694d6b28364
vggonzal <9Tcostoamm>
2023-08-15 22:24:39 -0700

    testing remote dynamodb

--- tests/example_load_data.py
+++ tests/example_load_data.py
@@ -4,37 +4,41 @@
 # In[1]:
 
 
-from pytest_dynamodb import factories
-
-
 import boto3
 import json
 from decimal import Decimal
 import geopandas as gpd
+import sys
 
 from hydrocron_database import Hydrocron_DB
 
+# ## Set up the connection to dynamoDB
 
-dynamo_test_proc = factories.dynamodb_proc(
-        dynamodb_dir="tests/dynamodb_local",
-        port=8000)
+# In[2]:
 
-dyndb_resource = factories.dynamodb("hydrocron_swot_reaches")
+
+session = boto3.session.Session(aws_access_key_id=sys.argv[1],
+                                aws_secret_access_key=sys.argv[2],
+                                aws_session_token='',
+                                region_name='us-west-2')
+
+dyndb_resource = session.resource('dynamodb', endpoint_url='http://localhost:8000')
+
 dynamo_instance = Hydrocron_DB(dyn_resource=dyndb_resource)
 
+# ## Create the table & load data
+
+# In[3]:
+print(len(sys.argv))
 
 table_name = 'hydrocron_swot_reaches'
 test_shapefile_path = 'tests/data/SWOT_L2_HR_RiverSP_Reach_548_011_NA_20230610T193337_20230610T193344_PIA1_01/SWOT_L2_HR_RiverSP_Reach_548_011_NA_20230610T193337_20230610T193344_PIA1_01.shp'
 
-print("list tables")
-print(dynamo_instance.tables.all())
-
 if dynamo_instance.table_exists(table_name):
-    print("table exists")
     dynamo_instance.delete_table(table_name)
 
 hydrocron_reach_table = dynamo_instance.create_table(table_name,
-                                                     partition_key='feature_id', partition_key_type='S',
+                                                     partition_key='reach_id', partition_key_type='S',
                                                      sort_key='time', sort_key_type='N')
 
 # read shapefile into geopandas dataframe
@@ -47,6 +51,11 @@ for index, row in shp_file.iterrows():
 
     # write to the table
     hydrocron_reach_table.add_data(**item_attrs)
+
+# ## test query
+
+# In[4]:
+
 
 items = hydrocron_reach_table.run_query(partition_key='71224100223')
 print(items)
