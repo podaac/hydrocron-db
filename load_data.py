78466ac6ac570c70126e849a9e64589f7fa06910
Victoria McDonald <victoria.mcdonald@jpl.nasa.gov>
2023-08-16 15:06:51 -0700

    add support to query cmr for granules

--- load_data.py
+++ load_data.py
@@ -2,15 +2,13 @@
 This module searches for new granules and loads data into
 the appropriate DynamoDB table
 '''
-import argparse
-from datetime import date
 import logging
 
 import boto3
 import earthaccess
-from hydrocron_database.hydrocron_database import HydrocronDB
-from hydrocron_database.hydrocron_database import DynamoKeys
-from hydrocron_database.io import swot_reach_node_shp
+from hydrocron_db.hydrocron_database import HydrocronDB
+from hydrocron_db.hydrocron_database import DynamoKeys
+from hydrocron_db.io import swot_reach_node_shp
 
 
 def setup_connection():
@@ -44,26 +42,25 @@ def find_new_granules(collection_shortname, start_date, end_date):
     ----------
     collection_shortname : string
         The shortname of the collection to search
-    
+
     Returns
     -------
     granule_paths : list of strings
         List of S3 paths to the granules that have not yet been ingested
     '''
-    earthaccess.login()
+    auth = earthaccess.login()
 
-    results = earthaccess.search_data(
-        short_name=collection_shortname,
-        cloud_hosted=True,
-        temporal=(start_date, end_date)
-    )
+    cmr_search = earthaccess.DataGranules(auth).\
+        short_name(collection_shortname).temporal(start_date, end_date)
+
+    results = cmr_search.get()
 
     granule_paths = [g.data_links(access='direct') for g in results]
-
+    print(granule_paths)
     return granule_paths
 
 
-def load_data(hydrocron_table, granule_paths):
+def load_data(hydrocron_table, granule_path):
     '''
     Create table and load data
 
@@ -73,53 +70,28 @@ def load_data(hydrocron_table, granule_paths):
         The list of S3 paths of granules to load data from
     '''
 
-    for granule in granule_paths:
-        if hydrocron_table == "hydrocron-swot-reach-table":
-            if 'Reach' in granule:
-                items = swot_reach_node_shp.read_shapefile(granule)
+    if hydrocron_table.table_name == "hydrocron-swot-reach-table":
+        if 'Reach' in granule_path:
+            items = swot_reach_node_shp.read_shapefile(granule_path)
 
-                for item_attrs in items:
-                    # write to the table
-                    hydrocron_table.add_data(**item_attrs)
+            for item_attrs in items:
+                # write to the table
+                hydrocron_table.add_data(**item_attrs)
 
-        elif hydrocron_table == "hydrocron-swot-node-table":
-            if 'Node' in granule:
-                items = swot_reach_node_shp.read_shapefile(granule)
+    elif hydrocron_table.table_name == "hydrocron-swot-node-table":
+        if 'Node' in granule_path:
+            items = swot_reach_node_shp.read_shapefile(granule_path)
 
-                for item_attrs in items:
-                    # write to the table
-                    hydrocron_table.add_data(**item_attrs)
+            for item_attrs in items:
+                # write to the table
+                hydrocron_table.add_data(**item_attrs)
 
-        else:
-            print('Items cannot be parsed, file reader not implemented')
-
-
-def create_parser():
-    '''
-    Creates argument parser
-    '''
-    parser = argparse.ArgumentParser(prog='PO.DAAC Hydrocron Database')
-
-    parser.add_argument(
-        "-tn", "--table-name",
-        dest="table_name",
-        required=True,
-        help="The name of the table to populate.")
-    parser.add_argument(
-        "-sd", "--start-date",
-        dest="start_date",
-        help="The ISO date after which data should be retrieved. For Example, --start-date 2023-06-14",  # noqa
-        default="2023-03-01")
-    parser.add_argument(
-        "-ed", "--end-date",
-        dest="end_date",
-        help="The ISO date before which data should be retrieved. For Example, --end-date 2023-07-14",  # noqa
-        default=date.today().strftime('%Y-%m-%d'))
-    
-    return parser
+    else:
+        print('Items cannot be parsed, file reader not implemented for table '
+              + hydrocron_table.table_name)
 
 
-def run(args=None):
+def run(table_name, start_date, end_date):
     '''
     Main function to manage loading data into Hydrocron
 
@@ -134,19 +106,12 @@ def run(args=None):
     end_date : string
         The end date to search new granules
     '''
-    if args is None:
-        parser = create_parser()
-        args = parser.parse_args()
-
-    table_name = args.table_name
-    start_date = args.start_date
-    end_date = args.end_date
 
     match table_name:
         case "hydrocron-swot-reach-table":
-            collection_shortname = "L2_HR_RiverSP"
+            collection_shortname = "SWOT_L2_HR_RIVERSP_1.0"
         case "hydrocron-swot-node-table":
-            collection_shortname = "L2_HR_RiverSP"
+            collection_shortname = "SWOT_L2_HR_RIVERSP_1.0"
         case _:
             logging.warning(
                 "Hydrocron table %(table_name) does not exist.", table_name)
@@ -165,8 +130,6 @@ def run(args=None):
 
         hydrocron_table = dynamo_instance.create_table(table_name, dynamo_keys)
 
-    table_name = 'hydrocron_swot_reaches'
-
     new_granules = find_new_granules(
         collection_shortname,
         start_date,
@@ -175,16 +138,13 @@ def run(args=None):
     load_data(hydrocron_table, new_granules)
 
 
-def main():
-    '''
-    Main function to run database loading
-    '''
+if __name__ == "__main__":
     try:
-        run()
+        run(
+            table_name='hydrocron-swot-reach-table',
+            start_date='2023-01-01T00:00:00',
+            end_date='2023-03-01T00:00:00')
+
     except Exception as e:  # pylint: disable=broad-except
         logging.exception("Uncaught exception occurred during execution.")
         exit(hash(e))
-
-
-if __name__ == "__main__":
-    main()
