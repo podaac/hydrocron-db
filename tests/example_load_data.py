0edcfe0a2a07e6a7d897a51364fe96129a18ecb2
vggonzal <9Tcostoamm>
2023-08-15 23:06:56 -0700

    testing remote dynamodb

--- tests/example_load_data.py
+++ tests/example_load_data.py
@@ -22,12 +22,11 @@ def upload():
         # convert each reach into a dictionary of attributes that dynamo can read
         item_attrs = json.loads(row.to_json(default_handler=str), parse_float=Decimal)
 
-        print(item_attrs)
         print("--------")
         # write to the table
         response = dynamodb.put_item(
             TableName='hydrocron_swot_reaches',
-            Item=row
+            Item=row.to_json(default_handler=str)
         )
     print(response)
 
