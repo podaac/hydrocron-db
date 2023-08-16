d4a5318f25d431f826e4bf2567351a5a1e515391
vggonzal <9Tcostoamm>
2023-08-15 21:29:37 -0700

    functioning local testing for dynamodb

--- tests/example_load_data.py
+++ tests/example_load_data.py
@@ -4,6 +4,10 @@
 # In[1]:
 
 
+from pytest_dynamodb import factories
+from tests.database_fixtures import hydrocron_dynamo_instance
+
+
 import boto3
 import json
 from decimal import Decimal
@@ -11,23 +15,15 @@ import geopandas as gpd
 
 from hydrocron_database import Hydrocron_DB
 
-# ## Set up the connection to dynamoDB
 
-# In[2]:
+__all__ = ['hydrocron_dynamo_instance']
 
+dynamo_test_proc = factories.dynamodb_proc(
+        dynamodb_dir="tests/dynamodb_local",
+        port=8000)
 
-session = boto3.session.Session(aws_access_key_id='a',
-                                aws_secret_access_key='a',
-                                aws_session_token='fake_session_token',
-                                region_name='us-west-2')
-
-dyndb_resource = session.resource('dynamodb', endpoint_url='http://localhost:8000')
-
+dyndb_resource = factories.dynamodb("hydrocron_swot_reaches")
 dynamo_instance = Hydrocron_DB(dyn_resource=dyndb_resource)
-
-# ## Create the table & load data
-
-# In[3]:
 
 
 table_name = 'hydrocron_swot_reaches'
@@ -37,7 +33,7 @@ if dynamo_instance.table_exists(table_name):
     dynamo_instance.delete_table(table_name)
 
 hydrocron_reach_table = dynamo_instance.create_table(table_name,
-                                                     partition_key='reach_id', partition_key_type='S',
+                                                     partition_key='feature_id', partition_key_type='S',
                                                      sort_key='time', sort_key_type='N')
 
 # read shapefile into geopandas dataframe
@@ -51,11 +47,6 @@ for index, row in shp_file.iterrows():
     # write to the table
     hydrocron_reach_table.add_data(**item_attrs)
 
-# ## test query
-
-# In[4]:
-
-
 items = hydrocron_reach_table.run_query(partition_key='71224100223')
-items
+print(items)
 
