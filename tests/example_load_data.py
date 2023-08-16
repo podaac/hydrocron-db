3c73c393b70af8054153c5a316919b08f13f2534
vggonzal <9Tcostoamm>
2023-08-16 00:03:07 -0700

    testing remote dynamodb

--- tests/example_load_data.py
+++ tests/example_load_data.py
@@ -19,31 +19,29 @@ def upload():
         print("ROW")
         object = {}
         for k, v in row.items():
-            print(k)
-            print(v)
             object[k] = {'S' : v}
         print(object)
         #print(type(row))
         print("^^^^^^^^")
         #print(row.to_json(default_handler=str))
 
-    test = {
-        'feature_id': {'S' : '71224100223'},
-        'area_total': {'S' : '-999999999999'},
-        'dschg_gcsf': {'S' : '-999999999999'},
-        'dschg_b': {'S' : '-999999999999'},
-        'dschg_gmsf': {'S' : '-999999999999'}
-    }
-    # convert each reach into a dictionary of attributes that dynamo can read
-    #item_attrs = json.loads(row.to_json(default_handler=str), parse_float=Decimal)
+        #test = {
+        #    'feature_id': {'S' : '71224100223'},
+        #    'area_total': {'S' : '-999999999999'},
+        #    'dschg_gcsf': {'S' : '-999999999999'},
+        #    'dschg_b': {'S' : '-999999999999'},
+        #    'dschg_gmsf': {'S' : '-999999999999'}
+        #}
+        # convert each reach into a dictionary of attributes that dynamo can read
+        #item_attrs = json.loads(row.to_json(default_handler=str), parse_float=Decimal)
 
-    print("--------")
-    # write to the table
-    response = dynamodb.put_item(
-        TableName='hydrocron_swot_reaches',
-        Item=test
-    )
-    print(response)
+        print("--------")
+        # write to the table
+        response = dynamodb.put_item(
+            TableName='hydrocron_swot_reaches',
+            Item=object
+        )
+        print(response)
 
 
 upload()
